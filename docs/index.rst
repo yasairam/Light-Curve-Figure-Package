31cf4c641987caa6e7c2c86611ef586624e5d3a7
Yashpranav <yashpranavsairam@gmail.com>
2026-06-24 11:27:27 -0700

    Documentation for light curve figure generator function

---
+++ docs/index.rst
@@ -0,0 +1,17 @@
+.. Light Curve Figure Package documentation master file, created by
+   sphinx-quickstart on Wed Jun 24 11:24:39 2026.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Light Curve Figure Package documentation
+========================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
--- lcfp/light_curve_constructor.py
+++ lcfp/light_curve_constructor.py
@@ -2,12 +2,13 @@
 def light_curve_generator(data):
     """Create a the light curve from the astronomical input.
 
-    Based on the input from the user in the CLI, this function will generate a publishable light curve.
+    Based on the input from the user in the CLI, this 
+        function will generate a publishable light curve.
 
     Args:
-        data (.csv file): csv (Comma Separated ) Reference against which cross
-            correlation is calculated.
+        data (.csv file): csv (Comma Separated Value file). Time-series data of 
+            light curve representable astronomical observation (e.g. Quasar/Transit/SNe)
 
     Returns:
-        array: cross-correlation function
+        None: light curve figure is saved to computer.
     """
