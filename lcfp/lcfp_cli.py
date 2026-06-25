73a6d24df98f4e94e3e5834d36ceb3ec6579b565
Kenneth Stebbing Cabrera <stebbingkenneth@gmail.com>
2026-06-24 21:12:07 -0700

    Rewrote as a function

--- lcfp/lcfp_cli.py
+++ lcfp/lcfp_cli.py
@@ -8,20 +8,43 @@ import argparse
 # ADD ARGUMENTS
 # ========================================================================================
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-type',  type=str, 
-                    help="output format:"+'\n'+"file [pdf], [png], [jpg], or interactive plot [itv]")
-parser.add_argument('-xaxis', type=str, 
-                    help="x-axis choice:"+'\n'+"magnitudes (AB) [mag], flux density [flux], Janskys [Jy], microJanskys [muJy], or nanoJanskys [nJy]")
-parser.add_argument('-yaxis', type=str,
-                    help="y-axis choice:"+'\n'+"Modified Julian Date [MJD], Julian Date [JD], seconds [sec], days [day], months [mon], or years [yr]")
-parser.add_argument('-scale', type='str',
-                    help="axis scaling:"+'\n'+"linear [lin], logarithmic [log], x-axis logarithmic [xlog], or y-axis logarithmic [ylog]")
-parser.add_argument('-date', type=str,
-                    help="calendar date on top axis:"+'\n'+"true [True] or false [False]")
-parser.add_argument('-xmin', type=float, help="min x value")
-parser.add_argument('-xmax', type=float, help="max x value")
-parser.add_argument('-ymin', type=float, help="min y value")
-parser.add_argument('-ymax', type=float, help="max y value")
+def parse_args():
+    """
+    Parse command-line arguments from the user.
+    
+    Returns:
+        Parsed command-line arguments.
+    """
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('-type',  
+                        type=str,
+                        choices=['pdf', 'png', 'jpg', 'itv'],
+                        default='itv',
+                        help="Output format: pdf, png, jpg, or interactive plot [itv]")
+    parser.add_argument('-xaxis', 
+                        type=str,
+                        choices=['mag', 'flux', 'Jy', 'muJy', 'nJy'],
+                        default='mag',
+                        help="x-axis choice: magnitudes (AB) [mag], flux density [flux], Janskys [Jy], microJanskys [muJy], or nanoJanskys [nJy]")
+    parser.add_argument('-yaxis', 
+                        type=str,
+                        choices=['MJD', 'JD', 'sec', 'day', 'month', 'year'],
+                        default='MJD',
+                        help="y-axis choice: Modified Julian Date [MJD], Julian Date [JD], seconds [sec], days [day], months [month], or years [year]")
+    parser.add_argument('-scale', 
+                        type=str,
+                        choices=['lin', 'log', 'xlog', 'ylog'],
+                        default='log',
+                        help="Axis scaling: linear [lin], logarithmic [log], x-axis logarithmic [xlog], or y-axis logarithmic [ylog]")
+    parser.add_argument('-date', 
+                        action='store_true',
+                        help="Show calendar date on top axis")
+    parser.add_argument('-xmin', type=float, help="Min x value")
+    parser.add_argument('-xmax', type=float, help="Max x value")
+    parser.add_argument('-ymin', type=float, help="Min y value")
+    parser.add_argument('-ymax', type=float, help="Max y value")
+    
+    return parser.parse_args()
 
-args = parser.parse_args()
